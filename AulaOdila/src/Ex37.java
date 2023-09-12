cb5420b5cfc3e07b329d70487b235b9e08f40257
Levi <leviteixeira225@gmail.com>
2023-09-12 10:04:10 -0300

    Quinto commit.1

--- AulaOdila/src/Ex37.java
+++ AulaOdila/src/Ex37.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Ex37 {
+import java.util.Scanner;
+
+public class Ex37 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        double volume, raio, altura;
+        System.out.print("Digite a altura da lata: ");
+        altura = num.nextDouble();
+        System.out.print("Digite o raio da lata: ");
+        raio = num.nextDouble();
+        volume = Math.PI * Math.pow(raio, 2) * altura;
+        System.out.println(String.format("O volume da sua lata é: %.2f", volume));
+    }
 }
